75e8c049c12d501e415ce4321c5359467d8c4658
hermany13 <hermanyvieiratj@gmail.com>
2020-12-15 11:24:08 -0300

    divided the routes by v1

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ProductsController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,14 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function() {
+    Route::post('login', [AuthController::class, 'authenticate']);
+
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::get('products', [ProductsController::class, 'index']);
+
+    Route::middleware('jwt.verify')->group(function (){
+        Route::post('products', [ProductsController::class, 'store']);
+
+        Route::put('products/{id}', [ProductsController::class, 'update']);
+
+        Route::delete('products/{id}', [ProductsController::class, 'destroy']);
+    });
 });
 
-Route::post('products', [ProductsController::class, 'store']);
 
-Route::get('products', [ProductsController::class, 'index']);
 
-Route::put('products/{id}', [ProductsController::class, 'update']);
 
-Route::delete('products/{id}', [ProductsController::class, 'destroy']);
